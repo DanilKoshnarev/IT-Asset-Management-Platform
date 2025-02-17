e479d6d2e56e8dc64f7266a284f40c62f10e7e83
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-17 12:42:52 +0200

    License.py

---
+++ src/domain/entities/License.py
@@ -0,0 +1,8 @@
+class License:
+    def __init__(self, id, asset_id, name, expiry_date):
+        self.id = id
+        self.asset_id = asset_id
+        self.name = name
+        self.expiry_date = expiry_date
+
+    # Getters and Setters
