214f59a1c83731bcae69622995862e7d48a16a41
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-17 12:42:04 +0200

    Asset.py

---
+++ src/domain/entities/Asset.py
@@ -0,0 +1,8 @@
+class Asset:
+    def __init__(self, id, name, type, status):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.status = status
+
+    # Getters and Setters
